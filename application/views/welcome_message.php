5a5cdf0fe61f6d60fb0949ed21a5e6cb44d61b80
Carl Jamilkowski <cdj255@nyu.edu>
2012-12-05 03:05:09 -0500

    Adding styling for front page

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,29 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 <head>
-	<meta charset="utf-8">
-	<title>Welcome</title>
+<meta charset="UTF-8">
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+<title>Ampulet</title>
+<style>
+body
+{
+	font-family:Arial, Helvetica, sans-serif;
+	color:#5b5b5b;
+}
+input
+{
+	-webkit-border-radius: 5px;
+	-moz-border-radius: 5px;
+	border-radius: 5px;
+}
+</style>
 </head>
-<body>
 
-<div id="container">
-	<div id="nav">
-    	<ul>
-        	<li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
+<body>
+<div id="Content" style="position: absolute; top: 281px; left: 310px; width: 500px;">
+    <div id="Logo" style="height:68px;">
+        <div style="width:51px; height:68px; position:absolute;">
+            <div style="background-color:#00ffff; width:11px; height:48px; top::0px; left:0px; position:absolute;"></div>
+            <div style="background-color:#00ffff; width:11px; height:48px; bottom:0px; left:20px; position:absolute;"></div>
+            <div style="background-color:#00ffff; width:11px; height:48px; bottom:0px; right:0px; position:absolute;"></div>
+        </div>
+        <div style="text-transform:lowercase; position:absolute; left:70px; line-height:68px; font-size:68px;">Ampulet</div>
     </div>
-    
-    <div id="content">
-    	<h1>Welcome to my site!</h1>
-        <p>Welcome to me awesome site </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nunc felis, tincidunt et consectetur quis, viverra a metus. Ut feugiat commodo neque sit amet gravida. Praesent at lacinia purus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In eleifend sollicitudin nunc vel tristique. Ut eget tortor justo, nec dapibus justo. Phasellus aliquam, felis sed consectetur pulvinar, turpis metus adipiscing leo, in iaculis turpis dui et augue. Proin et lorem nisl. Praesent eget iaculis ante. Integer sollicitudin tincidunt diam et malesuada. Mauris ac sapien ac nibh consectetur condimentum id quis neque. In eu mauris vitae elit tempor lobortis.</p>
-    </div>
-    
-    <div id="footer">
-    	<p>Copyright 2012 (c) All rights reserved.</p>
-    </div>
+    <h2>dragging healthcare kicking and screaming into the 21st century.</h2>
+    <p>ampulet is currently in private beta.<br>
+    sign up for a chance to get access.</p>
+    <form style="width:100%; position=absolute;">
+    	<input name="EmailText" id="EmailText" type="text" placeholder="enter your email address" style="left:0px; right:70px;position: absolute; ">
+        <input name="EmailSignupButton" id="EmailSignupButton" type="button" value="sign up" style="right:0px; width:65px;position: absolute; ">
+    </form>
 </div>
 </body>
 </html>
